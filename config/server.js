95d398b78ff7c538b36991c4596e75dcb970343e
Godwin Alexander <alecgee73@gmail.com>
2020-12-29 15:26:28 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5ae227726315d3a641378f962d9d3f72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '656ce766-744a-4cf8-a3d1-723243aa49f4'
+};
